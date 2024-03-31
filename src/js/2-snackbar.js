ebc6145389b20d918fca9d8d66d33a6853dd3004
Tatianadardik44 <dardiktatiana@gmail.com>
2024-03-31 21:03:18 +0200

    tuo-z

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,48 @@
-console.log("world");
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const formPromis = document.querySelector(".form");
+
+formPromis.addEventListener("submit", norma);
+function norma(event) {
+    event.preventDefault();
+    const delay = event.target.elements.delay.value; 
+    const delayNum = Number(delay);
+    const dovilne = event.target.state.value;
+    
+    const promis = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (dovilne === `fulfilled`) {
+                resolve(`✅ Fulfilled promise in ${delay}ms`);
+            } else  {
+                reject(`❌ Rejected promise in ${delay}ms`);
+            }
+        }, delayNum)
+        
+    });
+    
+    promis
+        .then((res) => iziToast.show({
+            message: res,
+            position: 'topRight',
+            backgroundColor: '#59a10d',
+            messageColor: '#fff',
+        })
+        
+    )
+         
+    .catch ((error) => iziToast.show({
+                   message: error,
+                   position: 'topRight',
+                   backgroundColor: '#ef4040',
+                   messageColor: '#fff',
+    }))
+    formPromis.reset();
+    
+};
+
+
+
+
+
